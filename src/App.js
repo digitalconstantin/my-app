d635f878cc1bdb955c78a81ae00004dfffbccb3f
Digital Constantin <digitalconstantin@gmail.com>
2020-10-01 21:45:00 +0200

    git push

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Constantin's Hello World Application</p>
-        <p>Ora sembra che è un pò meglio..</p>
+        <p>Oare s-a schimbat ceva?</p>
         <a
           className="App-link"
           href="https://reactjs.org"
