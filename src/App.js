56dd4f34acfbdc40e01450a90125a4a01b48db0f
digitalconstantin <digitalconstantin@gmail.com>
2020-10-01 15:17:03 +0200

    git push

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Constantin's Hello World Application</p>
-        <p>Sembra che funziona!</p>
+        <p>Sto cazzo di Visual Studio Code si blocca ancora???</p>
         <a
           className="App-link"
           href="https://reactjs.org"
